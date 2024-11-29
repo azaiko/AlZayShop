67441003d79ef224458edc625ef9f2e42814ab05
azaiko <azaiko72@gmail.com>
2024-11-29 10:43:04 +0300

    qq

---
+++ app/wait-for-db.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+until pg_isready -h db -p 5432; do
+  echo 'Waiting for database to be ready...'
+  sleep 2
+done
+echo 'Database is ready, starting Django...'
+python manage.py migrate
